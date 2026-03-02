3ba68ef5923d1c4d1a26ce121c7c5627bc234cbc
Mouna Mouhib <mouhib.mouna@ine.inpt.ac.ma>
2026-03-02 20:06:20 +0000

    Remove unused useEffect imports

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
